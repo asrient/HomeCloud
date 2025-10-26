d889beffabe542cc253c9349a0646ed245333be1
Asrient <44570278+asrient@users.noreply.github.com>
2025-10-26 16:17:54 +0530

    update env desktop

--- desktop/src/env.js
+++ desktop/src/env.js
@@ -9,8 +9,8 @@ const env = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     USE_WEB_APP_SERVER: true,
     UI_THEME: process.env.UI_THEME || null,
-    SERVER_URL: 'http://localhost:4000',
-    WS_SERVER_URL: 'ws://localhost:4000',
+    SERVER_URL: process.env.SERVER_URL || 'http://localhost:4000',
+    WS_SERVER_URL: process.env.WS_SERVER_URL || 'ws://localhost:4000',
     APP_ID: process.env.APP_ID || null,
 };
 
