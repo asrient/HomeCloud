2e4336c0c9683923c2f9eb17b181148708006f8c
Asrient <44570278+asrient@users.noreply.github.com>
2025-12-30 16:20:37 +0530

    update page title

--- mobile/app/(tabs)/(home)/index.tsx
+++ mobile/app/(tabs)/(home)/index.tsx
@@ -8,18 +8,34 @@ import { UIText } from '@/components/ui/UIText';
 import { ConnectionType } from '@/lib/types';
 import { UIHeaderButton } from '@/components/ui/UIHeaderButton';
 import { UIScrollView } from '@/components/ui/UIScrollView';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { useMemo } from 'react';
+
+const MAX_DEVICE_NAME_LENGTH = 23;
 
 export default function HomeScreen() {
   const router = useRouter();
+  const headerHeight = useHeaderHeight();
 
   const { selectedPeer, selectedPeerConnection } = useAppState();
+
+  const headerTitle = useMemo(() => {
+    const deviceName = selectedPeer ? selectedPeer.deviceName : 'This Device';
+    if (headerHeight > 150) {
+      return 'Media Center';
+    }
+    if (deviceName.length > MAX_DEVICE_NAME_LENGTH) {
+      return deviceName.substring(0, MAX_DEVICE_NAME_LENGTH - 3) + '...';
+    }
+    return deviceName;
+  }, [headerHeight, selectedPeer]);
 
   return (
     <UIScrollView style={{ flex: 1 }}>
       <Stack.Screen
         options={{
           title: 'Home',
-          headerTitle: 'Media Center',
+          headerTitle,
           headerLargeTitle: true,
           headerTransparent: true,
           headerRight: () =>
@@ -29,7 +45,7 @@ export default function HomeScreen() {
           ,
         }}
       />
-        <DeviceSelectorRow />
+        <DeviceSelectorRow/>
         <View style={styles.container}>
           <View style={{ alignItems: 'center', justifyContent: 'center', padding: 20 }}>
             <DeviceIcon size={200} iconKey={selectedPeer ? selectedPeer.iconKey : null} />
