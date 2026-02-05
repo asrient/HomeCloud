c2df66e075eaa9a33bf701a28e12057b7f8e9f3a
Asrient <44570278+asrient@users.noreply.github.com>
2026-02-06 04:05:06 +0530

    update splash screen

--- web/components/splashScreen.tsx
+++ web/components/splashScreen.tsx
@@ -4,6 +4,7 @@ import { ActionTypes } from '@/lib/state';
 import { Button } from './ui/button';
 import Image from 'next/image';
 import Head from 'next/head';
+import { cn, isWin11Theme } from '@/lib/utils';
 
 const SplashScreen = () => {
     const { appError } = useAppState();
@@ -14,27 +15,21 @@ const SplashScreen = () => {
                 HomeCloud
             </title>
         </Head>
-        <div className='w-full h-full min-h-screen p-4 flex flex-col justify-center items-center'>
+        <div className={cn('w-full h-full min-h-screen p-4 flex flex-col justify-center items-center app-dragable',
+            !isWin11Theme() && 'bg-background')}>
             <div>
-                <Image src='/icons/icon.png' priority alt='HomeCloud logo' width={appError ? 90 : 180} height={appError ? 90 : 180} />
+                <Image src='/icons/icon.png' priority alt='HomeCloud logo' width={appError ? 90 : 130} height={appError ? 90 : 130} />
             </div>
-            <div className='mt-10 text-foreground'>
-                {
-                    appError ? (<div className='flex flex-col justify-center items-center'>
-                        <div className='text-xl mb-2 font-medium'>
-                            {"Something went wrong :("}
-                        </div>
-                        <div className='text-foreground/70 font-mono text-xs'>{appError}</div>
-                        <div className='mt-3'>
-                            <Button size='lg' variant='default' onClick={() => window.location.reload()}>Try again</Button>
-                        </div>
-                    </div>) :
-                        <div className='text-xl mb-2 font-medium'>
-                            Setting things up..
-                        </div>
-                }
-            </div>
-
+            {appError &&
+                <div className='mt-6 text-foreground flex flex-col justify-center items-center app-nodrag'>
+                    <div className='text-xl mb-2 font-medium'>
+                        {"I got issues, you got'em too..."}
+                    </div>
+                    <div className='text-foreground/70 font-mono text-xs'>{appError}</div>
+                    <div className='mt-3'>
+                        <Button variant='default' onClick={() => window.location.reload()}>Reload</Button>
+                    </div>
+                </div>}
         </div>
     </>);
 };
