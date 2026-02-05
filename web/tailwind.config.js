ac6e817442de515f6f5122722e389db97a1bb44e
Asrient <44570278+asrient@users.noreply.github.com>
2026-02-05 08:14:24 +0530

    add lineheight macos

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -29,17 +29,17 @@ module.exports = {
 		...(isMacos ? {
 			// macOS typography guidelines (San Francisco font sizes)
 			fontSize: {
-				'xs': '0.72rem',
-				'sm': '0.8125rem',
-				'base': '0.88rem',
-				'lg': '1rem',
-				'xl': '1.125rem',
-				'2xl': '1.25rem',
-				'3xl': '1.5rem',
-				'4xl': '1.875rem',
-				'5xl': '2.25rem',
-				'6xl': '3rem',
-				'7xl': '4rem'
+				'xs': ['0.72rem', { lineHeight: '1rem' }],
+				'sm': ['0.8125rem', { lineHeight: '1.125rem' }],
+				'base': ['0.88rem', { lineHeight: '1.25rem' }],
+				'lg': ['1rem', { lineHeight: '1.5rem' }],
+				'xl': ['1.125rem', { lineHeight: '1.625rem' }],
+				'2xl': ['1.25rem', { lineHeight: '1.75rem' }],
+				'3xl': ['1.5rem', { lineHeight: '2rem' }],
+				'4xl': ['1.875rem', { lineHeight: '2.25rem' }],
+				'5xl': ['2.25rem', { lineHeight: '2.75rem' }],
+				'6xl': ['3rem', { lineHeight: '3.5rem' }],
+				'7xl': ['4rem', { lineHeight: '4.5rem' }]
 			}
 		} : {}
 		),
