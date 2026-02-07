1cdd695a232cc2034f3cee8e544a3158e2455e90
Asrient <44570278+asrient@users.noreply.github.com>
2026-02-08 00:10:28 +0530

    fix next js build

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,15 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
   },
-  output: 'export'
+  output: 'export',
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 }
 
 module.exports = nextConfig
